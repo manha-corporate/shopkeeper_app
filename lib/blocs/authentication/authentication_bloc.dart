020781ed38f8145dac6a683fa0815043d91ae478
manha-corporate <manha.corporate@gmail.com>
2025-05-02 23:39:16 +0500

    Add lib/blocs/authentication/authentication_bloc.dart 1746211154661

---
+++ lib/blocs/authentication/authentication_bloc.dart
@@ -0,0 +1,31 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import '../../repositories/authentication_repository.dart';
+
+part 'authentication_event.dart';
+part 'authentication_state.dart';
+
+class AuthenticationBloc extends Bloc<AuthenticationEvent, AuthenticationState> {
+  final AuthenticationRepository authenticationRepository;
+
+  AuthenticationBloc({required this.authenticationRepository}) : super(AuthenticationInitial()) {
+    on<AuthenticationStarted>((event, emit) async {
+      final isSignedIn = await authenticationRepository.isSignedIn();
+      if (isSignedIn) {
+        final user = await authenticationRepository.getUser();
+        emit(AuthenticationAuthenticated(user));
+      } else {
+        emit(AuthenticationUnauthenticated());
+      }
+    });
+
+    on<AuthenticationLoggedIn>((event, emit) {
+      emit(AuthenticationAuthenticated(event.user));
+    });
+
+    on<AuthenticationLoggedOut>((event, emit) async {
+      await authenticationRepository.signOut();
+      emit(AuthenticationUnauthenticated());
+    });
+  }
+}
