ae17fb144d7fd17af7efebceea24659c49a7e7bb
manha-corporate <manha.corporate@gmail.com>
2025-05-04 05:17:44 +0500

    Add lib/screens/profile_screen.dart 1746317862564

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('User Profile'),
+      ),
+      body: const Center(
+        child: Text('User profile and settings features will be implemented here.'),
+      ),
+    );
+  }
+}
